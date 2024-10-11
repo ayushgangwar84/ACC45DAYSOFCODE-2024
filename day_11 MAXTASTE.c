41c50926e7896d5319e56d79172d8f68a4289f14
ayushgangwar84 <ayushgangwar84330@gmail.com>
2024-10-11 11:23:00 +0530

    Add files via upload

---
+++ day_11 MAXTASTE.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T); 
+
+    while (T--) {
+        int a, b, c, d;
+        
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+
+      
+        int maxTastiness = (a > b ? a : b) + (c > d ? c : d);
+
+      
+        printf("%d\n", maxTastiness);
+    }
+
+    return 0;
+}
