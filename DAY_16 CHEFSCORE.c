0eb7f64a0e87ea2fa929452d26865b60642d46a3
ayushgangwar84 <ayushgangwar84330@gmail.com>
2024-10-21 22:26:48 +0530

    Add files via upload

---
+++ DAY_16 CHEFSCORE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    
+    while (T--) {
+        int N, X, Y;
+        scanf("%d %d %d", &N, &X, &Y);
+        
+        int maxScore = N * X;
+        
+        if (Y <= maxScore && Y % X == 0) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+    
+    return 0;
+}
+
