d289650cff49481b44af2e69e192748ac4436ae5
ayushgangwar84 <ayushgangwar84330@gmail.com>
2024-10-28 09:49:06 +0530

    Add files via upload

---
+++ Day23_CHN15A.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    
+    while (T--) {
+        int N, K; 
+        scanf("%d %d", &N, &K);
+        
+        int count = 0; 
+        for (int i = 0; i < N; ++i) {
+            int characteristic;
+            scanf("%d", &characteristic);
+            
+          
+            if ((characteristic + K) % 7 == 0) {
+                count++;
+            }
+        }
+        
+      
+        printf("%d\n", count);
+    }
+    
+    return 0;
+}
