2ecbe05c51adca845e73d5dc2f858699a212e02d
ayushgangwar84 <ayushgangwar84330@gmail.com>
2024-11-01 19:52:11 +0530

    Add files via upload

---
+++ Day27_TRUESCORE.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int T; 
+    scanf("%d", &T);
+    
+    while (T--) {
+        int A, B;
+        int C, D; 
+        
+      
+        scanf("%d %d", &A, &B);
+       
+        scanf("%d %d", &C, &D);
+        
+      
+        if (C >= A && D >= B) {
+            printf("POSSIBLE\n");
+        } else {
+            printf("IMPOSSIBLE\n");
+        }
+    }
+    
+    return 0;
+}
