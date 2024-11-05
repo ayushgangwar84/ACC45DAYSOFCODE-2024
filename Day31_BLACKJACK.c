949d86872da14a9d613848d09bc4bdb256fa7fe9
ayushgangwar84 <ayushgangwar84330@gmail.com>
2024-11-05 13:56:32 +0530

    Add files via upload

---
+++ Day31_BLACKJACK.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    
+   
+    scanf("%d", &T);
+    
+    while (T--) {
+        int A, B;
+       
+        scanf("%d %d", &A, &B);
+        
+    
+        int required = 21 - (A + B);
+        
+     
+        if (required >= 1 && required <= 10) {
+            printf("%d\n", required);
+        } else {
+            printf("-1\n");
+        }
+    }
+    
+    return 0;
+}
