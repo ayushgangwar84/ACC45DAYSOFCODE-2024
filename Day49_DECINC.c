da2e2c3f908f4fcc15f724d1fc33fd291a223131
ayushgangwar84 <ayushgangwar84330@gmail.com>
2024-11-24 13:21:27 +0530

    Add files via upload

---
+++ Day49_DECINC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    
+    scanf("%d", &N);
+
+    
+    if (N % 4 == 0) {
+        N++;  
+    } else {
+        N--;  
+    }
+
+    
+    printf("%d\n", N);
+
+    return 0;
+}
